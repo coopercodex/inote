e3fa778a6cf335219959cd867368d342898e9deb
Derek Cooper <derekcooper20@gmail.com>
2023-03-04 13:06:57 -0700

    deploy

--- index.js
+++ index.js
@@ -71,6 +71,10 @@ app.delete('/api/todos/:id', async (req, res) => {
   }
 })
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"))
+})
+
 app.listen(PORT, () => {
   console.log('Server has started on 4000')
 });
