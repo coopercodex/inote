7ac6210922950a61ef6c83e9b490dac16f643274
Derek Cooper <derekcooper20@gmail.com>
2023-03-04 01:09:09 -0700

    Server to root

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.post('/todos', async (req, res) => {
     const newTodo = await pool.query("INSERT INTO todo (title, description) VALUES($1, $2) RETURNING *",
       [title, description]
     );
-    res.json(newTodo.rows)
+    res.json(newTodo.rows[0])
   } catch (error) {
     console.log(error.message)
   }
@@ -22,7 +22,7 @@ app.post('/todos', async (req, res) => {
 
 app.get('/todos', async (req, res) => {
   try {
-    const allTodos = await pool.query("SELECT * FROM todo");
+    const allTodos = await pool.query("SELECT * FROM todo ORDER BY id DESC");
     res.json(allTodos.rows);
   } catch (error) {
     console.log(error.message);
@@ -33,7 +33,7 @@ app.get('/todos/:id', async (req, res) => {
   try {
     const { id } = req.params;
 
-    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id]);
+    const todo = await pool.query("SELECT * FROM todo WHERE id = $1", [id]);
     res.json(todo.rows[0]);
   } catch (error) {
     console.log(error.message)
@@ -45,7 +45,7 @@ app.put('/todos/:id', async (req, res) => {
     const { id } = req.params;
     const { title } = req.body;
     const { description } = req.body;
-    const updateTodo = await pool.query("UPDATE todo SET title = $1, description = $2 WHERE todo_id = $3",
+    const updateTodo = await pool.query("UPDATE todo SET title = $1, description = $2 WHERE id = $3",
       [title, description, id]);
     res.json("Todo was updated successfully")
   } catch (error) {
@@ -56,7 +56,7 @@ app.put('/todos/:id', async (req, res) => {
 app.delete('/todos/:id', async (req, res) => {
   try {
     const { id } = req.params;
-    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1",
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE id = $1",
       [id])
     res.json(`Todo was deleted successfully`)
   } catch (error) {
--- server/database.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE perntodo;
-
-CREATE TABLE todo(
-  todo_id SERIAL PRIMARY KEY,
-  title VARCHAR(255),
-  description VARCHAR(255)
-);  
